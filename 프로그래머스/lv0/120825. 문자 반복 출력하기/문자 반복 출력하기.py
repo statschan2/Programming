68d6335af58c5761ed9907fa2c44bafc3d586975
Lee Chan <125026241+statschan2@users.noreply.github.com>
2023-04-28 10:14:53 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,5 +1,2 @@
 def solution(my_string, n):
-    str_list = []
-    for i in list(my_string) :
-        str_list.append(i * n)
-    return ''.join(str_list)
+    return ''.join(i*n for i in list(my_string))
