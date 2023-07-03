8ae982c50004c6729ec4431f59acc99e6dbe00b2
Lee Chan <125026241+statschan2@users.noreply.github.com>
2023-07-03 15:51:47 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.py
@@ -1,6 +1,2 @@
 def solution(str_list, ex):
-    answer = []
-    for i in str_list :
-        if ex not in i :
-            answer.append(i)
     return ''.join([i for i in str_list if ex not in i])
