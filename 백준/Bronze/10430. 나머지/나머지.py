53e59a3357a7c46e8093f2e1e521db7abe4baf63
Lee Chan <125026241+statschan2@users.noreply.github.com>
2023-11-03 14:36:24 +0900

    [Bronze V] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.py
+++ 백준/Bronze/10430. 나머지/나머지.py
@@ -1,6 +1,14 @@
+# A, B, C
 a, b, c = map(int, input().split())
 
+# (A+B)%C
 print((a+b)%c)
-print(((a%c)+(b%c))%c)
+
+# ((A%C) + (B%C))%C
+print(((a%c) + (b%c))%c)
+
+# (A×B)%C
 print((a*b)%c)
-print(((a%c)*(b%c))%c)
+
+# ((A%C) × (B%C))%C
+print(((a%c) * (b%c))%c)
