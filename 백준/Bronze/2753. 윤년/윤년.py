bc52aabadd2e7ed4ba857dde24a9fe388dd74a14
Lee Chan <125026241+statschan2@users.noreply.github.com>
2023-11-06 12:00:27 +0900

    [Bronze V] Title: 윤년, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,6 +1,6 @@
+# 연도
 year = int(input())
 
-if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0) :
-    print(1)
-else :
-    print(0)
+# 윤년 확인
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0 : print(1)
+else : print(0)
